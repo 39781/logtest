26a625665047133479680ebaa9dd7a5e6f4cb17f
Your Name <you@example.com>
2018-06-25 14:04:01 +0530

    fc

--- routes.js
+++ routes.js
@@ -11,7 +11,7 @@ router.get('/close',function(req,res){
 })
 
 router.post('/botHandler',function(req, res){	
-	console.log(req..body.queryResult);
+	console.log(req.body.queryResult);
 	var actionName = req.body.queryResult.action;
 	switch(actionName){
 		case 'input.welcome':func = welcome;break;

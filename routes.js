a8b2891fd9695a8b19fa13ca74500e5a38c4f5b2
Your Name <you@example.com>
2018-06-25 16:35:17 +0530

    fc

--- routes.js
+++ routes.js
@@ -148,7 +148,7 @@ var listItem = function (response,text, items){
 	return new Promise(function(resolve,reject){		
 		console.log(' list item');
 			response.payload.google.systemIntent = {
-				"intent": "actions.intent.OPTION",
+				"intent": "actions.intent.TEXt",
 				"data": {
 					"@type": "type.googleapis.com/google.actions.v2.OptionValueSpec",
 					"listSelect": {
